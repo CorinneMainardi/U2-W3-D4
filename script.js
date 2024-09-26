a4b527e330f29ab7953a300fb3c77c37c78ccb46
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-09-26 21:07:02 +0200

    esercizio fino punto2

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const imagesApi = "NWBaV3jTf9mEbAcSdbHsybxNS6OLOu4wUcjZGl0qrOesDBeA8800Bl5k";
-const firstImagesUrl = "https://api.pexels.com//search?query=hamsters";
+const firstImagesUrl = "https://api.pexels.com/v1/search?query=hamsters";
 const secondImagesUrl = "https://api.pexels.com/v1/search?query=tigers";
+const genericUrl = "https://api.pexels.com/v1/search?query=";
 
-const getImages = () => {
-  fetch("https://api.pexels.com//search?query=hamsters", {
+const firstBtn = document.getElementById("firstBtn");
+const secondBtn = document.getElementById("secondBtn");
+let isHamsters;
+let query;
+
+const getImages = (query) => {
+  fetch(genericUrl + query, {
     headers: {
       Authorization: imagesApi,
     },
   })
     .then((response) => {
-      console.log("response", response);
       if (response.ok) {
-        return response.JSON;
+        // console.log("response", response);
+        return response.json();
       } else {
-        throw new Error("errore nella risposta");
+        throw new Error("errore nella richiesta");
       }
     })
-    .then((imagesData) => {
-      createCardWhitImages(imagesData);
-      console.log(imagesData);
+    .then((data) => {
+      console.log(data);
+      changeImg(data); //funzione scritta sotto
     })
     .catch((err) => {
-      console.log("error", err);
+      console.log(err);
     });
 };
 
-getImages();
+const changeImg = (data) => {
+  const imgDiv = document.querySelectorAll("img");
+  imgDiv.forEach((img, i) => {
+    img.src = data.photos[i].src.portrait;
+  });
+};
+
+firstBtn.addEventListener("click", getImages("hamsters"));
+firstBtn.addEventListener("click", getImages("tigers"));
